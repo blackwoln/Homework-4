b107d9597af9cf5ba6c6925266fd13442f0d4fae
vkray <vitalikkrainyuk@yandex.ru>
2023-04-10 18:47:33 +0300

    hm. Условный оператор. Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
 
+    public static void task1 () {
+        System.out.println("Задача 1");
+
+        int age = 25;
+        if (age >= 18) {
+            System.out.println("Он совершеннолетний");
+        } else {
+            System.out.println("Он не достиг совершеннолетия, нужно немного подождать");
+        }
+    }
+}
