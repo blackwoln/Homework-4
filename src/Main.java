66934b33757c85ca6356cb588de296ea25adc2a0
vkray <vitalikkrainyuk@yandex.ru>
2023-04-10 19:22:39 +0300

    hm3. Условный оператор. Задача 3 (Исправил вывод в предыдуших заданиях)

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,27 @@ public class Main {
     }
 
     public static void task1 () {
-        System.out.println("Задача 1");
+        System.out.println("Урок 1");
 
         int age = 25;
         if (age >= 18) {
-            System.out.println("Он совершеннолетний");
+            System.out.println("Если возраст человека равен " + age + ", он совершеннолетний");
         } else {
-            System.out.println("Он не достиг совершеннолетия, нужно немного подождать");
+            System.out.println("Если возраст человека равен " + age + ", он не достиг совершеннолетия, нужно немного подождать");
         }
 
         byte temperature = 15;
         if (temperature < 5) {
-            System.out.println("На улице холодно, нужно надеть шапку");
+            System.out.println("На улице " + temperature +  " градусов, нужно надеть шапку");
         } else {
-            System.out.println("Сегодня тепло, можно идти без шапки");
+            System.out.println("На улице " + temperature +  " градусов, можно идти без шапки");
         }
 
-
+        byte speed = 80;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + ", то придется заплатить штраф ");
+        } else {
+            System.out.println("Если скорость " + speed + ", можно ездить спокойно ");
+        }
     }
 }
